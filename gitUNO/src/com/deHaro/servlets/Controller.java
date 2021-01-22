c425ce59fd3247212e2210ab824970a5f6f898c3
picapinos <nachoharo44@gmail.com>
2021-01-22 13:17:52 +0100

    First commit. Creation commits

---
+++ gitUNO/src/com/deHaro/servlets/Controller.java
@@ -0,0 +1,35 @@
+ package com.deHaro.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String op=request.getParameter("op");
+		String url="";
+		switch(op) {
+			case "temaA":
+				request.getRequestDispatcher("TemaA").include(request, response);
+				url="temaA.jsp";
+				break;
+				
+			case "temaB":
+				request.getRequestDispatcher("TemaB").include(request, response);
+				url="temaB.jsp";
+				break;
+			
+			default:
+				url="menu.html";
+		}
+		
+		
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+
+}
